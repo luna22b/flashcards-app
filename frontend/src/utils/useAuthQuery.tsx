03375e11478a1a77d818decfbc6f9842ca1689e0
Brandon Luna <brandon.lunaj@gmail.com>
2026-05-21 22:15:30 -0400

    Just a query check to see if the user is authenticated

--- frontend/src/utils/useAuthQuery.tsx
+++ frontend/src/utils/useAuthQuery.tsx
@@ -1,6 +1,7 @@
 import { getMe } from "#/api/auth";
 import { useQuery } from "@tanstack/react-query";
 
+// this function is just for log out. maybe there will be more uses in the future
 export const useAuthQuery = () => {
   return useQuery({
     queryKey: ["auth"],
