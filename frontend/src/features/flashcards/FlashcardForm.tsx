06a8e2f9593c648fb2780ca7618bf759ead2bdc4
Brandon Luna <brandon.lunaj@gmail.com>
2026-05-22 11:07:01 -0400

    slowly adding ui after creating flashcards

--- frontend/src/features/flashcards/FlashcardForm.tsx
+++ frontend/src/features/flashcards/FlashcardForm.tsx
@@ -17,11 +17,6 @@ export default function FlashcardForm() {
       });
 
       console.log(newFlashcard);
-
-      setFlashcards({
-        front: "",
-        back: "",
-      });
     } catch (error) {
       console.error(error);
     }
--- frontend/src/routes/_authenticated/flashcards.tsx
+++ frontend/src/routes/_authenticated/flashcards.tsx
@@ -12,12 +12,13 @@ function RouteComponent() {
   return (
     <div>
       <Navbar />
-      <div className="mt-10 ml-80">
+      <div className="mt-10 ml-80 font-bold text-xl">
         Welcome to your flashcards, {user.username}!
       </div>
-      <div className="mt-2 ml-75">
+      <div className="mt-2 ml-75 text-gray-600">
         Start by creating your first flashcard below.
       </div>
+
       <FlashcardForm />
     </div>
   );
