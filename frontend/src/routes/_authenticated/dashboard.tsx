443a5e0d940b4eec1bdae4852970fa2bd49d9547
Brandon Luna <brandon.lunaj@gmail.com>
2026-05-21 22:14:10 -0400

    Added a test route to create flashcards

--- frontend/src/routes/_authenticated/dashboard.tsx
+++ frontend/src/routes/_authenticated/dashboard.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import Navbar from "#/components/Navbar";
 
 export const Route = createFileRoute("/_authenticated/dashboard")({
@@ -6,12 +6,19 @@ export const Route = createFileRoute("/_authenticated/dashboard")({
 });
 
 function RouteComponent() {
+  const navigate = useNavigate();
   const { user } = Route.useRouteContext() as { user: { username: string } };
 
   return (
     <div>
       <Navbar />
       <div>hello {user.username}!</div>
+      <button
+        onClick={() => navigate({ to: "/flashcards" })}
+        className="text-white bg-black rounded-xl mt-50 ml-50 cursor-pointer"
+      >
+        Go to flashcard creation route
+      </button>
     </div>
   );
 }
