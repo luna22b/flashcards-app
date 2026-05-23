e493faa0003a1f7627257b99cd9a5da1eae5314a
Brandon Luna <brandon.lunaj@gmail.com>
2026-05-22 22:13:00 -0400

    lots of things going, lemme tell ya

--- frontend/src/features/flashcards/types.ts
+++ frontend/src/features/flashcards/types.ts
@@ -1,24 +1,4 @@
-// this is for the flashcards that are sent back from the backend as a response
-export interface Flashcard {
-  id: string;
-  front: string;
-  back: string;
-  createdAt: string;
-  userId: string;
-}
-
-// this is for the forms
 export interface CreateFlashcardInput {
   front: string;
   back: string;
-}
-
-// this is also for the forms, but specifically for the main page. passes a prop
-export interface FlashcardFormProps {
-  onCardCreated: (newCard: any) => void;
-}
-
-// another prop for the flashcards list
-export interface FlashcardCardProps {
-  card: Flashcard;
 }
--- frontend/src/routes/_authenticated/flashcards.tsx
+++ frontend/src/routes/_authenticated/flashcards.tsx
@@ -1,9 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
-import Navbar from "#/components/Navbar";
-import FlashcardForm from "#/features/flashcards/FlashcardForm";
-import FlashcardList from "#/features/flashcards/FlashcardList";
 import { useState } from "react";
-import type { Flashcard } from "#/features/flashcards/types";
+import axios from "axios";
+import type { CreateFlashcardInput } from "#/features/flashcards/types";
+import Navbar from "#/components/Navbar";
 
 export const Route = createFileRoute("/_authenticated/flashcards")({
   component: RouteComponent,
@@ -11,23 +10,84 @@ export const Route = createFileRoute("/_authenticated/flashcards")({
 
 function RouteComponent() {
   const { user } = Route.useRouteContext() as { user: { username: string } };
-  const [cards, setCards] = useState<Flashcard[]>([]);
 
-  const handleCardCreated = (newCard: Flashcard) => {
-    setCards((prevCards) => [newCard, ...prevCards]);
+  const [flashcards, setFlashcards] = useState<CreateFlashcardInput>({
+    front: "",
+    back: "",
+  });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/flashcards",
+        flashcards,
+        {
+          withCredentials: true,
+        },
+      );
+
+      console.log("FLASHCARD CREATED:", response.data);
+
+      setFlashcards({
+        front: "",
+        back: "",
+      });
+    } catch (error) {
+      console.error("CREATE FLASHCARD ERROR:", error);
+    }
   };
 
   return (
     <div>
       <Navbar />
+
       <div className="mt-10 ml-80 font-bold text-xl">
         Welcome to your flashcards, {user.username}!
       </div>
+
       <div className="mt-2 ml-75 text-gray-600">
         Start by creating your first flashcard below.
       </div>
-      <FlashcardForm onCardCreated={handleCardCreated} />
-      <FlashcardList cards={cards} />
+
+      <form onSubmit={handleSubmit}>
+        <div className="ml-85 mt-25">Type your title</div>
+
+        <input
+          type="text"
+          value={flashcards.front}
+          onChange={(e) =>
+            setFlashcards({
+              ...flashcards,
+              front: e.target.value,
+            })
+          }
+          className="border p-2 rounded bg-amber-100 ml-80"
+          required
+        />
+
+        <div className="ml-80 mt-10">Type the description</div>
+
+        <textarea
+          value={flashcards.back}
+          onChange={(e) =>
+            setFlashcards({
+              ...flashcards,
+              back: e.target.value,
+            })
+          }
+          className="rounded bg-amber-100 ml-75 w-80 h-40 border p-4"
+          required
+        />
+
+        <button
+          type="submit"
+          className="mt-10 ml-95 bg-black text-white rounded-sm p-2 cursor-pointer"
+        >
+          Create flashcard
+        </button>
+      </form>
     </div>
   );
 }
