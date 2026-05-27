eccc5fe72562b6892ea6c050bf954549ee03de41
Brandon Luna <brandon.lunaj@gmail.com>
2026-05-27 12:32:53 -0400

    changed up some route names

---
+++ frontend/src/routes/_authenticated/flashcards/create.tsx
@@ -0,0 +1,152 @@
+import { createFileRoute } from "@tanstack/react-router";
+import Navbar from "#/components/Navbar";
+import { useState } from "react";
+import { FlashcardField } from "#/features/flashcards/FlashcardField";
+import axios from "axios";
+import { useNavigate } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/_authenticated/flashcards/create")({
+  component: RouteComponent,
+});
+
+// interfaces
+interface Flashcard {
+  id: string;
+  front: string;
+  back: string;
+}
+
+interface FlashcardSetForm {
+  title: string;
+  description: string;
+}
+
+function RouteComponent() {
+  const [titleAndDesc, setTitleAndDesc] = useState<FlashcardSetForm>({
+    title: "",
+    description: "",
+  });
+
+  // initialized with two flashcards at the start so that the user has two flashcards to work with
+  // automatically
+  const [flashcards, setFlashcards] = useState<Flashcard[]>([
+    { id: crypto.randomUUID(), front: "", back: "" },
+    { id: crypto.randomUUID(), front: "", back: "" },
+  ]);
+
+  // handles the adding of a flashcard by adding a new object to the state
+  const handleAddFlashcard = () => {
+    const newFlashcard = { id: crypto.randomUUID(), front: "", back: "" };
+    setFlashcards([...flashcards, newFlashcard]);
+  };
+
+  // handles updating in the other file by lifting state up
+  const handleUpdate = (
+    id: string,
+    field: "front" | "back",
+    newValue: string,
+  ) => {
+    setFlashcards((prevCards) =>
+      prevCards.map((card) =>
+        card.id === id ? { ...card, [field]: newValue } : card,
+      ),
+    );
+  };
+
+  // handles deletion by filtering the id of the card that was clicked on
+  const handleDeleteFlashcard = (id: string) => {
+    setFlashcards((c) => c.filter((card) => card.id !== id));
+  };
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const filteredFlashcards = flashcards
+      .filter((item) => item.front.trim() !== "" && item.back.trim() !== "")
+      .map(({ front, back }) => ({
+        front,
+        back,
+      }));
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/flashcards",
+        {
+          flashcards: filteredFlashcards,
+          title: titleAndDesc.title,
+          description: titleAndDesc.description,
+        },
+        {
+          withCredentials: true,
+        },
+      );
+      navigate({ to: "/flashcards" });
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <div className="text-xl mt-10 ml-20">Create a new flashcard set</div>
+      <div className="mt-10 mb-2 ml-20">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={titleAndDesc.title}
+            onChange={(e) =>
+              setTitleAndDesc((prev) => ({ ...prev, title: e.target.value }))
+            }
+            placeholder="Title"
+            required
+            className="w-[50em] border mb-2"
+          />
+
+          <div>
+            <input
+              type="text"
+              value={titleAndDesc.description}
+              onChange={(e) =>
+                setTitleAndDesc((prev) => ({
+                  ...prev,
+                  description: e.target.value,
+                }))
+              }
+              placeholder="Description"
+              className="w-[50em] border"
+            />
+          </div>
+          <div>
+            <ul>
+              {flashcards.map((card) => (
+                <li key={card.id}>
+                  <FlashcardField
+                    card={card}
+                    onUpdate={(field, val) => handleUpdate(card.id, field, val)}
+                    onClick={() => handleDeleteFlashcard(card.id)}
+                  />
+                </li>
+              ))}
+            </ul>
+            <button
+              onClick={handleAddFlashcard}
+              className="ml-20 cursor-pointer"
+              type="button"
+            >
+              Add new flashcard
+            </button>
+            <button
+              type="submit"
+              className="border-green-400 border mt-5 cursor-pointer"
+            >
+              Save & Continue
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/routes/_authenticated/flashcards/index.tsx
@@ -0,0 +1,45 @@
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import Navbar from "#/components/Navbar";
+import { getFlashcards } from "#/api/flashcard-api";
+
+export const Route = createFileRoute("/_authenticated/flashcards/")({
+  beforeLoad: async () => {
+    const flashcards = await getFlashcards();
+
+    return { flashcards: flashcards ?? [] };
+  },
+
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  const navigate = useNavigate();
+  const { user } = Route.useRouteContext() as { user: { username: string } };
+  const { flashcards } = Route.useRouteContext();
+
+  return (
+    <div>
+      <Navbar />
+      <div>Welcome {user.username}!</div>
+      <div>
+        These are all your flashcards! To make a new flashcard, create a new set
+        below!
+      </div>
+
+      <div className="gap-5 grid mt-20 ml-5 cursor-pointer">
+        {flashcards.map((card: any) => (
+          <div key={card.id} className="border w-[40em]">
+            {card.title}
+          </div>
+        ))}
+      </div>
+
+      <button
+        onClick={() => navigate({ to: "/flashcards/create" })}
+        className="text-white bg-black rounded-xl mt-50 ml-50 cursor-pointer"
+      >
+        Create a new set
+      </button>
+    </div>
+  );
+}
