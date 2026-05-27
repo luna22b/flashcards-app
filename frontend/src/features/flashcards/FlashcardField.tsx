af7319d1ab000bf5c40c7850f1210927a01116ef
Brandon Luna <brandon.lunaj@gmail.com>
2026-05-26 22:37:52 -0400

    Add onUpdate that lifts state up

--- frontend/src/features/flashcards/FlashcardField.tsx
+++ frontend/src/features/flashcards/FlashcardField.tsx
@@ -5,15 +5,35 @@ type Props = {
     back: string;
   };
   onClick: () => void;
+  onUpdate: (field: "front" | "back", newValue: string) => void;
 };
 
-export function FlashcardField({ card, onClick }: Props) {
+export function FlashcardField({ card, onClick, onUpdate }: Props) {
   return (
     <div>
-      <div className="ml-20 mt-5 w-[50em] h-20 border">
-        {card.front} {card.back}
+      <div className="mt-5 w-[50em] h-25 border">
         <div>
-          <button onClick={onClick} className="cursor-pointer">
+          <input
+            type="text"
+            value={card.front}
+            onChange={(e) => onUpdate("front", e.target.value)}
+            className="border w-full p-1"
+            placeholder="Enter term"
+          />
+        </div>
+
+        <div>
+          <input
+            type="text"
+            value={card.back}
+            onChange={(e) => onUpdate("back", e.target.value)}
+            className="border w-full p-1"
+            placeholder="Enter definition"
+          />
+        </div>
+
+        <div>
+          <button type="button" onClick={onClick} className="cursor-pointer">
             Delete
           </button>
         </div>
