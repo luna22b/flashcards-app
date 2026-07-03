847eb35517729cad8f3df5164c7f47916b65cbaf
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-03 11:47:58 -0400

    creating multiple choice quiz option

--- frontend/src/routes/_authenticated/flashcards/$setId/test.tsx
+++ frontend/src/routes/_authenticated/flashcards/$setId/test.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { getSpecificCard } from "#/api/flashcard-api";
+import Navbar from "#/components/Navbar";
 
 export const Route = createFileRoute("/_authenticated/flashcards/$setId/test")({
   loader: async ({ params }) => {
@@ -13,17 +14,22 @@ export const Route = createFileRoute("/_authenticated/flashcards/$setId/test")({
 
 function RouteComponent() {
   const flashcardSet = Route.useLoaderData();
+  console.log(flashcardSet);
   return (
     <div>
-      {flashcardSet.flashcards.map((card: any) => (
-        <div
-          key={card.id}
-          className="bg-gray-700 text-white rounded-lg w-[27em] text-center h-55 border-4 border-black"
-        >
-          <div>{card.front}</div>
-          <div>{card.back}</div>
-        </div>
-      ))}
+      <Navbar />
+      <div className="flex flex-col items-center justify-center mt-10 gap-5">
+        {flashcardSet.flashcards.map((card: any) => (
+          <div
+            key={card.id}
+            className="bg-gray-700 text-white rounded-lg w-[27em] text-center h-55 border-4 border-black"
+          >
+            <div>{card.front}</div>
+            <div>Choose an answer</div>
+            <div>{card.back}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
