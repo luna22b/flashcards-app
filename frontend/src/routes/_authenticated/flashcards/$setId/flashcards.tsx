efcf1a45800763721566ebf6654a4c6e5eaf9752
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-05 22:40:58 -0400

    adding flashcards to the website

---
+++ frontend/src/routes/_authenticated/flashcards/$setId/flashcards.tsx
@@ -0,0 +1,19 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { getSpecificCard } from "#/api/flashcard-api";
+import Navbar from "#/components/Navbar";
+
+export const Route = createFileRoute(
+  "/_authenticated/flashcards/$setId/flashcards",
+)({
+  loader: async ({ params }) => {
+    const flashcardSet = await getSpecificCard(params.setId);
+
+    return flashcardSet;
+  },
+
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return <div></div>;
+}
